f1fb7c87f22372d2305887793499ef5c25aa6ff4
moeabdol <admin.r99@gmail.com>
2015-11-28 06:04:09 +0300

    add argparse to oweather script

--- oweather.py
+++ oweather.py
@@ -2,41 +2,13 @@
 import requests
 import json
 import ujson
+import argparse
 from argparse import ArgumentParser
+import ipdb
 
 class OpenWeatherMapAPIWrapper:
   def __init__(self, api_key):
     self.api_key = api_key
-    args = self.process_arguments()
-    self.get_weather_info(args)
-
-  def process_arguments(self):
-    parser = ArgumentParser(description="Get weather information from the " +
-                                        "Open Weather Map API")
-    forecast_group = parser.add_mutually_exclusive_group()
-    forecast_group.add_argument("-f",
-                                "--forecast",
-                                dest="forecast",
-                                action="store_true",
-                                help="Get five day weather forcast for city")
-    forecast_group.add_argument("-d",
-                                "--days",
-                                dest="n",
-                                type=int,
-                                help="Get N days weather forcast for city")
-    parser.add_argument("city",
-                        help="Name of city you want to get weather for",
-                        nargs=1)
-    return parser.parse_args()
-
-  def get_weather_info(self, args):
-    if args.forecast is True:
-      self.print_to_screen(self.get_5day_forcast_by_city_name(args.city[0]))
-    elif args.n > 0:
-      self.print_to_screen(self.get_daily_forcast_by_city_name(args.city[0],
-                                                               days=args.n))
-    else:
-      self.print_to_screen(self.get_current_weather_by_city_name(args.city[0]))
 
   def get_current_weather_by_city_name(self, city_name):
     end_point = "http://api.openweathermap.org/data/2.5/weather"
@@ -61,11 +33,60 @@ class OpenWeatherMapAPIWrapper:
                             appid_query)
     return ujson.loads(response.text)
 
-  def print_to_screen(self, weather_info_json):
-    print json.dumps(weather_info_json, sort_keys=True, indent=2,
-                     separators=(",", ": 2"))
+def process_arguments():
+  parser = ArgumentParser(description="Get weather information from the " +
+                                      "Open Weather Map API")
+  forecast_group = parser.add_mutually_exclusive_group()
+  forecast_group.add_argument("-f",
+                              "--forecast",
+                              dest="forecast",
+                              action="store_true",
+                              help="Get five day weather forcast for city")
+  forecast_group.add_argument("-d",
+                              "--days",
+                              dest="n",
+                              type=int,
+                              help="Get N days weather forcast for city")
+  key_group = parser.add_mutually_exclusive_group(required=True)
+  key_group.add_argument("-k",
+                      "--key",
+                      type=str,
+                      help="Open weather map API key")
+  key_group.add_argument("--key-file",
+                         dest="file",
+                         type=argparse.FileType("r"),
+                         help="Text file with open weather map API key")
+  parser.add_argument("city",
+                      help="Name of city you want to get weather for",
+                      nargs=1)
+  return parser.parse_args()
+
+def print_weather_info(owmaw, args):
+  if args.forecast is True:
+    print_json(owmaw.get_5day_forcast_by_city_name(args.city[0]))
+  elif args.n > 0:
+    print_json(owmaw.get_daily_forcast_by_city_name(args.city[0], days=args.n))
+  else:
+    print_json(owmaw.get_current_weather_by_city_name(args.city[0]))
+
+def print_json(json_dict):
+  print json.dumps(json_dict, sort_keys=True, indent=2,
+                    separators=(",", ": 2"))
+
+def get_api_key_from_file(f):
+  for line in f.readlines():
+    key, value = line.partition("=")[::2]
+    if key.strip() == "openweathermap_api_key":
+      return value.strip()[1:-1]
+
+def get_api_key(args):
+  if args.key is not None:
+    return args.key
+  elif args.file is not None:
+    return get_api_key_from_file(args.file)
 
 if __name__ == "__main__":
-  from utils import get_api_keys
-  owm_api_key = get_api_keys("api_keys.txt")["openweathermap_api_key"]
-  owmaw = OpenWeatherMapAPIWrapper(owm_api_key)
+  args = process_arguments()
+  api_key = get_api_key(args)
+  owmaw = OpenWeatherMapAPIWrapper(api_key)
+  print_weather_info(owmaw, args)
